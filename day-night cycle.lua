aaaec8d5ef723e890622f13af7666d3d996e92d9
Stanulet Sebastian <vexorwarlock@gmail.com>
2025-12-01 13:29:05 +0200

    Real Life Synchronised Day-Night Cycle
    Added a second loop to update Lighting.TimeOfDay based on UTC.

---
+++ day-night cycle.lua
@@ -0,0 +1,14 @@
+local Lighting=game:GetService("Lighting")
+
+
+--pe timpul romaniei
+while true do
+    task.wait(1)
+    Lighting.TimeOfDay=os.date("%H:%M:%S", os.time())
+end
+
+--pe timpul UTC
+while true do
+    task.wait(1)
+    Lighting.TimeOfDay=os.date("%H:%M:%S", os.time()-10800)
+end
